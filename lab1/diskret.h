11ce93f1104490006414b95bab708849ce78cf87
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-02-23 21:49:16 +0700

    Update diskret.h

--- lab1/diskret.h
+++ lab1/diskret.h
@@ -1,1 +1,20 @@
+#ifndef DISKRET_H
+#define DISKRET_H
+
+#define MAX_SIZE 50
+
+// Ввод и обработка множества
+int input_set(const char* name, char* set);
+
+// Операции над множествами (алгоритм слияния)
+int union_sets(char* A, int sizeA, char* B, int sizeB, char* result);
+int intersect_sets(char* A, int sizeA, char* B, int sizeB, char* result);
+int difference_sets(char* A, int sizeA, char* B, int sizeB, char* result);
+int is_subset(char* A, int sizeA, char* B, int sizeB);
+
+// Вспомогательные функции
+void print_set(const char* name, char* set, int size);
+void clear_input();
+
+#endif
 
